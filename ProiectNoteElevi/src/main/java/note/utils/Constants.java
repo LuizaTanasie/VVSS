b5ccd484de8d5c1946f2eebad1ebef1624f131d8
luizatanasie <luiza.tanasie@gmail.com>
2018-04-03 18:00:28 +0300

    lab 2

--- ProiectNoteElevi/src/main/java/note/utils/Constants.java
+++ ProiectNoteElevi/src/main/java/note/utils/Constants.java
@@ -3,8 +3,8 @@ package note.utils;
 public class Constants {
 	public static int minNota = 1;
 	public static int maxNota = 10;
-	public static int minNrmatricol = 1;
-	public static int maxNrmatricol = 1000;
+	public static int minNrmatricol = 0;
+	public static int maxNrmatricol = Integer.MAX_VALUE;
 	public static String invalidNota = "Nota introdusa nu este corecta";
 	public static String invalidMateria = "Lungimea materiei este invalida";
 	public static String invalidNrmatricol = "Numarul matricol introdus nu este corect";
--- ProiectNoteElevi/src/test/java/note/test/AddNotaTest.java
+++ ProiectNoteElevi/src/test/java/note/test/AddNotaTest.java
@@ -15,181 +15,107 @@ import note.utils.Constants;
 import note.controller.NoteController;
 
 public class AddNotaTest {
-	
+
 	private NoteController ctrl;
-	
+
 	@Before
 	public void init(){
 		ctrl = new NoteController();
 	}
-	
+
 	@Rule
 	public ExpectedException expectedEx = ExpectedException.none();
 
 	@Test
-	public void test1() throws ClasaException {
-		Nota nota = new Nota(1, "Desen", 10);
-		ctrl.addNota(nota);
-		assertEquals(1, ctrl.getNote().size());
-	}
-	
-	@Test
-	public void test2() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidNrmatricol);
-		Nota nota = new Nota(10.1, "Istorie", 5);
-		ctrl.addNota(nota);
-	}
-	
-	@Test
-	public void test3() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidNrmatricol);
-		Nota nota = new Nota(0, "Istorie", 5);
-		ctrl.addNota(nota);
-	}
-	
-	@Test
-	public void test4() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidMateria);
-		Nota nota = new Nota(1, "Isto", 5);
-		ctrl.addNota(nota);
-	}
-	
-	@Test
-	public void test5() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidNota);
-		Nota nota = new Nota(1, "Istorie", 5.002);
-		ctrl.addNota(nota);
-	}
-	
-	@Test
-	public void test6() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidNota);
-		Nota nota = new Nota(1, "Istorie", 11);
-		ctrl.addNota(nota);
-	}
-	
-	@Test
-	public void test7() throws ClasaException {
-		Nota nota = new Nota(2, "Istorie", 10);
-		ctrl.addNota(nota);
-		assertEquals(1, ctrl.getNote().size());
-	}
-	
-	@Test
-	public void test8() throws ClasaException {
-		Nota nota = new Nota(1000, "Istorie", 10);
-		ctrl.addNota(nota);
-		assertEquals(1, ctrl.getNote().size());
-	}
-	
-	@Test
-	public void test9() throws ClasaException {
-		Nota nota = new Nota(999, "Istorie", 10);
-		ctrl.addNota(nota);
-		assertEquals(1, ctrl.getNote().size());
-	}
-	
-	@Test
-	public void test10() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidNrmatricol);
-		Nota nota = new Nota(1001, "Istorie", 5);
-		ctrl.addNota(nota);
-	}
-	
-	@Test
-	public void test11() throws ClasaException {
-		Nota nota = new Nota(1000, "Desena", 10);
+	public void TC01_ECP() throws ClasaException {
+		Nota nota = new Nota(1, "materie", 8);
 		ctrl.addNota(nota);
 		assertEquals(1, ctrl.getNote().size());
 	}
 
 	@Test
-	public void test12() throws ClasaException {
-		Nota nota = new Nota(1000, "DesenDesenDesenDesen", 10);
+	public void TC06_ECP() throws ClasaException {
+		expectedEx.expect(ClasaException.class);
+		expectedEx.expectMessage(Constants.invalidNrmatricol);
+		Nota nota = new Nota(-1, "materie", 8);
 		ctrl.addNota(nota);
-		assertEquals(1, ctrl.getNote().size());
 	}
-	
+
 	@Test
-	public void test13() throws ClasaException {
-		Nota nota = new Nota(1000, "DesenDesenDesenDese", 10);
+	public void TC07_ECP() throws ClasaException {
+		expectedEx.expect(ClasaException.class);
+		expectedEx.expectMessage(Constants.invalidNrmatricol);
+		Nota nota = new Nota(1.5, "materie", 8);
+		ctrl.addNota(nota);
+	}
+
+	@Test
+	public void TC04_ECP() throws ClasaException {
+		expectedEx.expect(ClasaException.class);
+		expectedEx.expectMessage(Constants.invalidNota);
+		Nota nota = new Nota(1, "materie", 0);
+		ctrl.addNota(nota);
+	}
+
+	@Test
+	public void TC03_ECP() throws ClasaException {
+		expectedEx.expect(ClasaException.class);
+		expectedEx.expectMessage(Constants.invalidNota);
+		Nota nota = new Nota(1, "materie", 4.5);
+		ctrl.addNota(nota);
+	}
+
+	@Test
+	public void TC05_ECP() throws ClasaException {
+		expectedEx.expect(ClasaException.class);
+		expectedEx.expectMessage(Constants.invalidNota);
+		Nota nota = new Nota(1, "materie", 11);
+		ctrl.addNota(nota);
+	}
+
+	@Test
+	public void TC11_BV() throws ClasaException {
+		Nota nota = new Nota(4, "materie", 10);
 		ctrl.addNota(nota);
 		assertEquals(1, ctrl.getNote().size());
 	}
 
-	
 	@Test
-	public void test14() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidMateria);
-		Nota nota = new Nota(1, "DesenDesenDesenDesenD", 5);
-		ctrl.addNota(nota);
-	}
-	
-	@Test
-	public void test15() throws ClasaException {
-		Nota nota = new Nota(1000, "DesenDesenDesenDese", 1);
-		ctrl.addNota(nota);
-		assertEquals(1, ctrl.getNote().size());
-	}
-	
-	@Test
-	public void test16() throws ClasaException {
-		Nota nota = new Nota(1000, "DesenDesenDesenDese", 2);
-		ctrl.addNota(nota);
-		assertEquals(1, ctrl.getNote().size());
-	}
-	
-	@Test
-	public void test17() throws ClasaException {
-		Nota nota = new Nota(1000, "DesenDesenDesenDese", 9);
-		ctrl.addNota(nota);
-		assertEquals(1, ctrl.getNote().size());
-	}
-	
-	@Test
-	public void test18() throws ClasaException {
+	public void TC09_BV() throws ClasaException {
 		expectedEx.expect(ClasaException.class);
 		expectedEx.expectMessage(Constants.invalidNota);
-		Nota nota = new Nota(1, "Istorie", 0);
+		Nota nota = new Nota(1, "materie", -1);
 		ctrl.addNota(nota);
 	}
-	
+
 	@Test
-	public void test19() throws ClasaException {
+	public void TC16_BV() throws ClasaException {
+		Nota nota = new Nota(Integer.MAX_VALUE, "materie", 5);
+		ctrl.addNota(nota);
+		assertEquals(1, ctrl.getNote().size());
+	}
+
+	@Test
+	public void TC17_BV() throws ClasaException {
+		Nota nota = new Nota(Integer.MAX_VALUE-1, "materie", 5);
+		ctrl.addNota(nota);
+		assertEquals(1, ctrl.getNote().size());
+	}
+
+	@Test
+	public void TC01_BV() throws ClasaException {
 		expectedEx.expect(ClasaException.class);
 		expectedEx.expectMessage(Constants.invalidMateria);
 		Nota nota = new Nota(1, "", 5);
 		ctrl.addNota(nota);
 	}
-	
+
 	@Test
-	public void test20() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidMateria);
-		Nota nota = new Nota(1, "a", 5);
+	public void TC04_BV() throws ClasaException {
+		Nota nota = new Nota(1, "Materieeeeeeeeeee123", 5);
 		ctrl.addNota(nota);
+		assertEquals(1, ctrl.getNote().size());
 	}
-	
-	@Test
-	public void test21() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidNota);
-		Nota nota = new Nota(1, "Istorie", -2);
-		ctrl.addNota(nota);
-	}
-	
-	@Test
-	public void tes22() throws ClasaException {
-		expectedEx.expect(ClasaException.class);
-		expectedEx.expectMessage(Constants.invalidNrmatricol);
-		Nota nota = new Nota(-1001, "Istorie", 5);
-		ctrl.addNota(nota);
-	}
+
+
 }
